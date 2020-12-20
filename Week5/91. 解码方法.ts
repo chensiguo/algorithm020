d0b1f738a7e70d3342c928a434fd57934daa413a
chensiguo <97318206@qq.com>
2020-12-20 23:53:43 +0800

    第5周作业

---
+++ Week5/91. 解码方法.ts
@@ -0,0 +1,22 @@
+function numDecodings(s: string): number {
+  if (s.length === 0 || s[0] === '0') return 0;
+
+  const dp = new Array(s.length + 1).fill(0);
+
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= s.length; i++) {
+    const a = Number(s.slice(i - 1, i));
+    if (a >= 1 && a <= 9) {
+      dp[i] += dp[i - 1];
+    }
+
+    const b = Number(s.slice(i - 2, i));
+    if (b >= 10 && b <= 26) {
+      dp[i] += dp[i - 2];
+    }
+  }
+
+  return dp[s.length];
+};
